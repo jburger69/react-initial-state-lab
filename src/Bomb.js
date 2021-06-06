2d198b12f03c8b84839c59a9c3d8bcffa8c05c93
Joshua Burger <joshberger6969@outlook.com>
2021-06-06 11:35:33 -0400

    commit tree 359b83106a45c4fde7092d965862948af72251e3

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,20 @@
+import React from 'react';
+
 // your Bomb code here!
+export default class Bomb extends React.Component {
+
+    constructor () {
+        super()
+        this.state = {
+            currentSlideIndex: 0
+        }
+    }
+
+
+    render() {
+      return (
+          <h1>I am on slide {this.state.currentSlideIndex}</h1>
+      )
+    }
+
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,22 @@
+import React from 'react';
 // your ImageSlider code here!
+export default class ImageSlider extends React.Component {
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+
+    render() {
+      const message = this.state.secondsLeft > 0 ?
+      `${this.state.secondsLeft} seconds left before I go boom!` : 'Boom!'
+      return (
+          <div>
+              {message}
+          </div>
+      )
+    }
+
+}
